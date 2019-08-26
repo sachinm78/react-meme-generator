f202d04bf0792042dedfb9fbc19daf54cfa30485
sachinm78 <sachinm78@gmail.com>
2019-08-26 14:13:23 -0500

    completed lesson

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -10,6 +10,7 @@ class MemeGenerator extends Component {
             allMemeImgs: []
         }
         this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
     
     componentDidMount() {
@@ -20,16 +21,23 @@ class MemeGenerator extends Component {
                 this.setState({ allMemeImgs: memes })
             })
     }
-     
+    
     handleChange(event) {
         const {name, value} = event.target
         this.setState({ [name]: value })
     }
     
+    handleSubmit(event) {
+        event.preventDefault()
+        const randNum = Math.floor(Math.random() * this.state.allMemeImgs.length)
+        const randMemeImg = this.state.allMemeImgs[randNum].url
+        this.setState({ randomImg: randMemeImg })
+    }
+    
     render() {
         return (
             <div>
-                <form className="meme-form">
+                <form className="meme-form" onSubmit={this.handleSubmit}>
                     <input 
                         type="text"
                         name="topText"
