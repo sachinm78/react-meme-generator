4d8fa321ba2e2ab962362ea3b82cb92ba5081c91
sachinm78 <sachinm78@gmail.com>
2019-08-26 14:03:18 -0500

    added simple form

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -6,13 +6,48 @@ class MemeGenerator extends Component {
         this.state = {
             topText: "",
             bottomText: "",
-            randomImg: "http://i.imgflip.com/1bij.jpg"
+            randomImg: "http://i.imgflip.com/1bij.jpg",
+            allMemeImgs: []
         }
     }
     
+    componentDidMount() {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(response => response.json())
+            .then(response => {
+                const {memes} = response.data
+                console.log(memes[0])
+                this.setState({ allMemeImgs: memes })
+            })
+    }
+    
+    /**
+     * Create the onChagne handler method
+     * It should update the corresponding state on every change of the input box
+     */
+    
     render() {
         return (
-            <h1>MEME GENERATOR SECTION</h1>
+            <div>
+                <form className="meme-form">
+                    <input 
+                        type="text"
+                        name="topText"
+                        placeholder="Top Text"
+                        value={this.state.topText}
+                        // onChange={???}
+                    /> 
+                    <input 
+                        type="text"
+                        name="bottomText"
+                        placeholder="Bottom Text"
+                        value={this.state.bottomText}
+                        // onChange={???}
+                    /> 
+                
+                    <button>Gen</button>
+                </form>
+            </div>
         )
     }
 }
