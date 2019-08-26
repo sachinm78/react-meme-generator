cda798b55ac3ec56ebfa6a68979668fc1daf9700
sachinm78 <sachinm78@gmail.com>
2019-08-26 13:55:39 -0500

    created header and successfully rendered

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from "./Header"
+// import MemeGenerator from "./MemeGenerator"
 
 function App() {
-  return (
-      <h1>Hello world!</h1>
-  )
+    return (
+        <div>
+            <Header />
+            {/* <MemeGenerator /> */}
+        </div>
+    )
 }
 
 export default App
